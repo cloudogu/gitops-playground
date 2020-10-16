c71ab4947c65eb385fca44f6ab5ce2b3fa386c32
Philipp Markiewka <philipp.markiewka@cloudogu.com>
2020-10-16 09:59:20 +0200

    Adds scm-manager environment
    Signed-off-by: Philipp Markiewka <philipp.markiewka@cloudogu.com>

--- README.md
+++ README.md
@@ -15,3 +15,23 @@ helm upgrade --install jenkins --values jenkins/values.yaml jenkins/jenkins
 ```
 
 Find jenkins on http://localhost:9090
+
+## SCM-Manager
+
+### Copy chart to k3s static folder
+
+```
+sudo cp scm-manager/scm-manager-2.7.1.tgz  /var/lib/rancher/k3s/server/static/charts/
+```
+
+### Copy manifests to k3s manifests folder
+
+```
+sudo cp scm-manager/*.yaml /var/lib/rancher/k3s/server/manifests/
+```
+
+### Start
+
+Find scm-manager on http://localhost:9091
+
+Login with scmadmin/scmadmin
---
+++ scm-manager/initscmm-curl.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+#set -x
+
+SCM_USER=scmadmin
+SCM_PWD=scmadmin
+HOST=localhost:8080
+
+function main() {
+
+  cd /tmp && wget https://github.com/dtschan/curl-static/releases/download/v7.63.0/curl && chmod +x curl
+  while [[ "$(./curl -s -L -o /dev/null -w ''%{http_code}'' "http://${HOST}/scm")" -ne "200" ]]; do sleep 5; done;
+
+  setConfig "namespaceStrategy" "CustomNamespaceStrategy"
+  addRepo "cluster" "gitops"
+  addUser "${JENKINS_USERNAME}" "${JENKINS_PASSWORD}" "jenkins@mail.de"
+  setPermission "cluster" "gitops" "${JENKINS_USERNAME}" "WRITE"
+
+  rm curl
+}
+
+function addRepo() {
+  ./curl -i -L -X POST -H "Content-Type: application/vnd.scmm-repository+json;v=2" \
+    --data "{\"name\":\"${2}\",\"namespace\":\"${1}\",\"type\":\"git\",\"contact\":\"admin@mail.de\",\"description\":\"description\",\"contextEntries\":{},\"_links\":{}}" \
+    "http://${SCM_USER}:${SCM_PWD}@${HOST}/scm/api/v2/repositories"
+}
+
+function setConfig() {
+  ./curl -i -L -X PUT -H "Content-Type: application/vnd.scmm-config+json;v=2" \
+    --data "{\"${1}\": \"${2}\"}" \
+    "http://${SCM_USER}:${SCM_PWD}@${HOST}/scm/api/v2/config"
+}
+
+function addUser() {
+  ./curl -i -L -X POST -H "Content-Type: application/vnd.scmm-user+json;v=2" \
+    --data "{\"name\":\"${1}\",\"displayName\":\"${1}\",\"mail\":\"${3}\",\"password\":\"${2}\",\"active\":true,\"_links\":{}}" \
+    "http://${SCM_USER}:${SCM_PWD}@${HOST}/scm/api/v2/users"
+}
+
+function setPermission() {
+  ./curl -i -L -X POST -H "Content-Type: application/vnd.scmm-repositoryPermission+json" \
+    --data "{\"name\":\"${3}\",\"role\":\"${4}\",\"verbs\":[],\"groupPermission\":false}" \
+    "http://${SCM_USER}:${SCM_PWD}@${HOST}/scm/api/v2/repositories/${1}/${2}/permissions/"
+}
+
+main "$@"; exit
