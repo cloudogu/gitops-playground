2dbbcb18ec55efc779fbc980691a3b90e62c5275
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2020-11-05 13:10:51 +0100

    Install SCMM and Jenkins Plugins.
    Allows for builds triggered on push.

--- scm-manager/initscmm.sh
+++ scm-manager/initscmm.sh
@@ -23,6 +23,7 @@ function main() {
   addRepo "application" "petclinic-plain"
   setPermission "application" "petclinic-plain" "${JENKINS_USERNAME}" "WRITE"
 
+  configJenkins
   rm curl
 }
 
@@ -34,7 +35,7 @@ function addRepo() {
 
 function setConfig() {
   ./curl -i -L -X PUT -H "Content-Type: application/vnd.scmm-config+json;v=2" \
-    --data "{\"proxyPassword\":null,\"proxyPort\":8080,\"proxyServer\":\"proxy.mydomain.com\",\"proxyUser\":null,\"enableProxy\":false,\"realmDescription\":\"SONIA :: SCM Manager\",\"disableGroupingGrid\":false,\"dateFormat\":\"YYYY-MM-DD HH:mm:ss\",\"anonymousAccessEnabled\":false,\"anonymousMode\":\"OFF\",\"baseUrl\":\"http://localhost:8080/scm\",\"forceBaseUrl\":false,\"loginAttemptLimit\":-1,\"proxyExcludes\":[],\"skipFailedAuthenticators\":false,\"pluginUrl\":\"https://plugin-center-api.scm-manager.org/api/v1/plugins/{version}?os={os}&arch={arch}\",\"loginAttemptLimitTimeout\":300,\"enabledXsrfProtection\":true,\"namespaceStrategy\":\"CustomNamespaceStrategy\",\"loginInfoUrl\":\"https://login-info.scm-manager.org/api/v1/login-info\",\"releaseFeedUrl\":\"https://scm-manager.org/download/rss.xml\",\"mailDomainName\":\"scm-manager.local\",\"_links\":{\"self\":{\"href\":\"http://localhost:9091/scm/api/v2/config\"},\"update\":{\"href\":\"http://localhost:9091/scm/api/v2/config\"}},\"adminGroups\":[],\"adminUsers\":[]}" \
+    --data "{\"proxyPassword\":null,\"proxyPort\":8080,\"proxyServer\":\"proxy.mydomain.com\",\"proxyUser\":null,\"enableProxy\":false,\"realmDescription\":\"SONIA :: SCM Manager\",\"disableGroupingGrid\":false,\"dateFormat\":\"YYYY-MM-DD HH:mm:ss\",\"anonymousAccessEnabled\":false,\"anonymousMode\":\"OFF\",\"baseUrl\":\"http://scmm-scm-manager:9091/scm\",\"forceBaseUrl\":false,\"loginAttemptLimit\":-1,\"proxyExcludes\":[],\"skipFailedAuthenticators\":false,\"pluginUrl\":\"https://plugin-center-api.scm-manager.org/api/v1/plugins/{version}?os={os}&arch={arch}\",\"loginAttemptLimitTimeout\":300,\"enabledXsrfProtection\":true,\"namespaceStrategy\":\"CustomNamespaceStrategy\",\"loginInfoUrl\":\"https://login-info.scm-manager.org/api/v1/login-info\",\"releaseFeedUrl\":\"https://scm-manager.org/download/rss.xml\",\"mailDomainName\":\"scm-manager.local\",\"_links\":{\"self\":{\"href\":\"http://localhost:9091/scm/api/v2/config\"},\"update\":{\"href\":\"http://localhost:9091/scm/api/v2/config\"}},\"adminGroups\":[],\"adminUsers\":[]}" \
     "http://${SCM_USER}:${SCM_PWD}@${HOST}/scm/api/v2/config"
 }
 
@@ -48,6 +49,12 @@ function setPermission() {
   ./curl -i -L -X POST -H "Content-Type: application/vnd.scmm-repositoryPermission+json" \
     --data "{\"name\":\"${3}\",\"role\":\"${4}\",\"verbs\":[],\"groupPermission\":false}" \
     "http://${SCM_USER}:${SCM_PWD}@${HOST}/scm/api/v2/repositories/${1}/${2}/permissions/"
+}
+
+function configJenkins() {
+  ./curl -i -L -X PUT -H 'Content-Type: application/json' \
+    --data-raw '{"disableRepositoryConfiguration":false,"disableMercurialTrigger":false,"disableGitTrigger":false,"disableEventTrigger":false,"url":"http://jenkins:8080"}' \
+    "http://${SCM_USER}:${SCM_PWD}@${HOST}/scm/api/v2/config/jenkins/"
 }
 
 main "$@"; exit
