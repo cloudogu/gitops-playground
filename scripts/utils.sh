850eeefe1666d359c2900569703b0f58720bf9d3
Marek Markiewka <marek.markiewka@cloudogu.com>
2020-11-23 15:05:59 +0100

    removed unnecessary method in utils
    Signed-off-by: Marek Markiewka <marek.markiewka@cloudogu.com>

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -16,17 +16,6 @@ function confirm() {
   esac
 }
 
-function loading() {
-  spin='-\|/'
-  i=0
-  while kill -0 $1 2>/dev/null
-  do
-    i=$(( (i+1) %4 ))
-    printf "\r${spin:$i:1}"
-    sleep .1
-  done
-}
-
 function spinner() {
     local info="$1"
     local pid=$!
