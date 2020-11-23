2fde822143ba123f7d9ccb66d27b5ee5ba663263
Marek Markiewka <marek.markiewka@cloudogu.com>
2020-11-23 15:03:50 +0100

    refined destroy script
    Signed-off-by: Marek Markiewka <marek.markiewka@cloudogu.com>

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -7,37 +7,71 @@ ABSOLUTE_BASEDIR="$(cd ${BASEDIR} && pwd)"
 
 source ${ABSOLUTE_BASEDIR}/utils.sh
 
-function main() {
+function removeFluxv1() {
+  helm delete flux-operator -n fluxv1 || true
+  helm delete helm-operator -n fluxv1 || true
+}
+
+function removeFluxv2() {
   # The following line is needed because the cr "fluxv2-kustomizer" has a finalizer set which can lead to a deadlock while deleting
   # https://stackoverflow.com/a/52012367
   kubectl patch kustomization fluxv2-kustomizer -p '{"metadata":{"finalizers":[]}}' --type=merge -n fluxv2 || true
   kubectl delete -f fluxv2/clusters/k8s-gitops-playground/fluxv2/gotk-kustomization.yaml || true
   kubectl delete -f fluxv2/clusters/k8s-gitops-playground/fluxv2/gotk-gitrepository.yaml || true
   kubectl delete -f fluxv2/clusters/k8s-gitops-playground/fluxv2/gotk-components.yaml || true
+}
 
-  helm delete scmm -n default || true
-  helm delete jenkins -n default || true
-  helm delete flux-operator -n default || true
-  helm delete helm-operator -n default || true
-  helm delete docker-registry -n default || true
-
-  kubectl delete -f jenkins/resources || true
-  kubectl delete -f scm-manager/resources || true
-  kubectl delete secret gitops-scmm || true
-
-  kubectl delete -f k8s-namespaces/ || true
-
+function removeArgoCD() {
   kubectl delete -f argocd/resources || true
-  helm delete argocd -n default || true
+  helm delete argocd -n argocd || true
+}
 
-  #cleanup
+function removeSCMM() {
+  helm delete scmm -n default || true
+  kubectl delete -f scm-manager/resources || true
+}
+
+function removeJenkins() {
+  helm delete jenkins -n default || true
+  kubectl delete -f jenkins/resources || true
+}
+
+function removeK8sResources() {
+  helm delete docker-registry -n default || true
+  kubectl delete secret gitops-scmm || true
+  kubectl delete -f k8s-namespaces/ || true
+}
+
+function cleanup () {
   kubectl delete crd/helmreleases.helm.fluxcd.io || true
   kubectl delete customresourcedefinition.apiextensions.k8s.io/applications.argoproj.io || true
   kubectl delete customresourcedefinition.apiextensions.k8s.io/appprojects.argoproj.io || true
   kubectl delete apiservice.apiregistration.k8s.io/v1alpha1.argoproj.io || true
   kubectl delete appproject.argoproj.io/default || true
+}
 
-  confirm "" 'Remove Jenkins agent workspace in this folder as well? y/n [n]' && rm -rf /tmp/k8s-gitops-playground-jenkins-agent
+function main() {
+  VERBOSE=$1
+
+  if [[ $VERBOSE = false ]]; then
+    removeFluxv1
+    removeFluxv2
+    removeArgoCD
+    removeSCMM
+    removeJenkins
+    removeK8sResources
+    cleanup
+  elif [[ $VERBOSE = true ]]; then
+    removeFluxv1 > /dev/null 2>&1 & spinner "Removing Flux V1"
+    removeFluxv2 > /dev/null 2>&1 & spinner "Removing Flux V2"
+    removeArgoCD > /dev/null 2>&1 & spinner "Removing ArgoCD"
+    removeSCMM > /dev/null 2>&1 & spinner "Removing SCM-Manager"
+    removeJenkins > /dev/null 2>&1 & spinner "Removing Jenkins"
+    removeK8sResources > /dev/null 2>&1 & spinner "Removing other K8s Resources"
+    cleanup > /dev/null 2>&1 & spinner "Cleaning up"
+  fi
+
+  confirm 'Remove Jenkins agent workspace in this folder as well? y/n [n]' && rm -rf /tmp/k8s-gitops-playground-jenkins-agent
 }
 
 function printUsage()
@@ -50,13 +84,39 @@ function printUsage()
 
 function printParameters() {
     echo "The following parameters are valid"
-    echo "-h --help   - Help screen"
-    echo "-d --debug  - Debug output"
+    echo "-h | --help     >> Help screen"
+    echo "-v | --verbose  >> Verbose output"
+    echo "-d | --debug    >> Debug output"
 }
 
+COMMANDS=$(getopt \
+                -o hvd \
+                --long help,verbose,debug \
+                -- "$@")
+
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+eval set -- "$COMMANDS"
+
+VERBOSE=false
+DEBUG=false
+while true; do
+  case "$1" in
+    -h | --help     ) printUsage; exit 0 ;;
+    -v | --verbose  ) VERBOSE=true; shift ;;
+    -d | --debug    ) DEBUG=true; shift ;;
+    --              ) shift; break ;;
+    *               ) break ;;
+  esac
+done
+
 confirm "Removing gitops playground from kubernetes cluster: '$(kubectl config current-context)'." 'Continue? y/n [n]' ||
-exit 0
+  exit 0
 
-main "$@"
-
-#main "$@" > /dev/null 2>&1 & spinner "Removing all Cloudogu GitOps Playground resources..."
+if [[ $DEBUG = true ]]; then
+  main false
+elif [[ $VERBOSE = true ]]; then
+  main $VERBOSE
+else
+  main $VERBOSE > /dev/null 2>&1 & spinner "Removing all Cloudogu GitOps Playground resources..."
+fi
