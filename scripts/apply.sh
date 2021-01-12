3b7c315f4561bb5ed7995aa988e58ec55687015f
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2021-01-12 21:07:52 +0100

    Set all service ports to 80.
    Allows for conveniently accessing them without having to remember port numbers on remote cluster.
    
    This makes a lot of LB services stay on pending state on local k3s cluster, but this does not seem to have disadvantages, because the services can still be reached via nodePort.

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -5,16 +5,13 @@ set -o errexit -o nounset -o pipefail
 BASEDIR=$(dirname $0)
 ABSOLUTE_BASEDIR="$(cd ${BASEDIR} && pwd)"
 PLAYGROUND_DIR="$(cd ${BASEDIR} && cd .. && pwd)"
-JENKINS_HOME="/tmp/k8s-gitops-playground-jenkins-agent"
 
 PETCLINIC_COMMIT=949c5af
+SPRING_BOOT_HELM_CHART_COMMIT=3515518e
+
 # get scm-manager port from values
-SCMM_PORT=$(grep -A1 'service:' "${PLAYGROUND_DIR}"/scm-manager/values.yaml | tail -n1 | cut -f2 -d':' | tr -d '[:space:]')
+SCMM_PORT=$(grep -A5 'service:' "${PLAYGROUND_DIR}"/scm-manager/values.yaml | grep nodePort | tail -n1 | cut -f2 -d':' | tr -d '[:space:]')
 SCMM_IP="localhost"
-
-PETCLINIC_COMMIT=949c5af
-# get scm-manager port from values
-SCMM_PORT=$(grep -A1 'service:' scm-manager/values.yaml | tail -n1 | cut -f2 -d':' | tr -d '[:space:]')
 
 source ${ABSOLUTE_BASEDIR}/utils.sh
 
@@ -107,7 +104,7 @@ function initSCMM() {
 function initJenkins() {
   # Find out the docker group and put the agent into it. Otherwise it has no permission to access  the docker host.
   helm upgrade -i jenkins --values jenkins/values.yaml \
-    $(helmSettingsForLocalCluster) --set agent.runAsGroup=$(queryDockerGroupOfJenkinsNode) \
+    $(jenkinsHelmSettingsForLocalCluster) --set agent.runAsGroup=$(queryDockerGroupOfJenkinsNode) \
     --version 2.13.0 jenkins/jenkins -n default
 }
 
@@ -123,7 +120,7 @@ function queryDockerGroupOfJenkinsNode() {
   kubectl delete -f jenkins/tmp-docker-gid-grepper.yaml >/dev/null &
 }
 
-function helmSettingsForLocalCluster() {
+function jenkinsHelmSettingsForLocalCluster() {
   if [[ $REMOTE_CLUSTER != true ]]; then
     # Run Jenkins and Agent pods as the current user.
     # Avoids file permission problems when accessing files on the host that were written from the pods
@@ -156,7 +153,9 @@ function initFluxV2() {
 }
 
 function initArgo() {
-  helm upgrade -i argocd --values argocd/values.yaml --version 2.9.5 argo/argo-cd -n argocd
+  helm upgrade -i argocd --values argocd/values.yaml \
+    $(argoHelmSettingsForLocalCluster) --version 2.9.5 argo/argo-cd -n argocd
+
   kubectl apply -f argocd/resources -n argocd || true
 
   BCRYPT_PW=$(bcryptPassword "${SET_PASSWORD}")
@@ -166,6 +165,13 @@ function initArgo() {
   pushPetClinicRepo 'applications/petclinic/argocd/plain-k8s' 'argocd/petclinic-plain'
   initRepo 'argocd/gitops'
   initRepoWithSource 'applications/nginx/argocd' 'argocd/nginx-helm'
+}
+
+function argoHelmSettingsForLocalCluster() {
+  if [[ $REMOTE_CLUSTER != true ]]; then
+    # Can't set service nodePort for argo, so change service ports explicitly  
+    echo '--set server.service.servicePortHttp=9092 --set server.service.servicePortHttps=9093' 
+  fi
 }
 
 function createSecrets() {
@@ -216,7 +222,13 @@ function pushHelmChartRepo() {
   git clone -n https://github.com/cloudogu/spring-boot-helm-chart.git "${TMP_REPO}" --quiet
   (
     cd "${TMP_REPO}"
+    # Checkout a defined commit in order to get a deterministic result
+    git checkout ${SPRING_BOOT_HELM_CHART_COMMIT} --quiet
+    # Create a defined version to use in demo applications
     git tag 1.0.0
+    
+    git branch -d main
+    git branch main
 
     waitForScmManager
     git push "http://${SET_USERNAME}:${SET_PASSWORD}@${SCMM_IP}:${SCMM_PORT}/scm/repo/${TARGET_REPO_SCMM}" HEAD:main --force --quiet
