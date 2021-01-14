ded93d87aae2cf510e5d453eea0ee4696587f133
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2021-01-14 10:27:39 +0100

    Fix empty repos in remote cluster

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -41,6 +41,10 @@ function main() {
   
   evalWithSpinner applyBasicK8sResources "Basic setup & starting registry..."
   evalWithSpinner initSCMM "Starting SCM-Manager..."
+  
+  # We need to query remote IP here (in the main process) again, because the "initSCMM" methods might be running in a 
+  # background process (to display the spinner only)
+  setExternalHostnameIfNecessary 'scmm' 'scmm-scm-manager' 'default'
 
   if [[ $INSTALL_ALL_MODULES = true || $INSTALL_FLUXV1 = true ]]; then
     evalWithSpinner initFluxV1 "Starting Flux V1..."
