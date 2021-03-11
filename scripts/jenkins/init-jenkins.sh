6d5ee8d0c08836d71d0a6b0538cb5e547a0490ff
thoppenheidt <timo.hoppenheidt@cloudogu.com>
2021-03-11 11:44:55 +0100

    removed debugging echo

--- scripts/jenkins/init-jenkins.sh
+++ scripts/jenkins/init-jenkins.sh
@@ -64,7 +64,6 @@ function waitForJenkins() {
   HTTP_CODE="0"
   while [[ "${HTTP_CODE}" -ne "200" ]]; do
     HTTP_CODE="$(curl -s -L -o /dev/null -w ''%{http_code}'' "${JENKINS_URL}/login")" || true
-    echo ${HTTP_CODE}
     echo -n "."
     sleep 2
   done
