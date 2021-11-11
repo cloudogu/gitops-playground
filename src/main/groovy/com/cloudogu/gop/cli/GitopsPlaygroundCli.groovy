ed6ac66e3dfdbc12efd2327aee48cc2956aae8b5
Frederick Roy <frederick.roy@cloudogu.com>
2021-11-11 17:52:11 +0100

    make use of groovy commons cli instead of picocli

--- src/main/groovy/com/cloudogu/gop/cli/GitopsPlaygroundCli.groovy
+++ src/main/groovy/com/cloudogu/gop/cli/GitopsPlaygroundCli.groovy
@@ -1,24 +1,61 @@
 package com.cloudogu.gop.cli
 
-import io.micronaut.configuration.picocli.PicocliRunner
-import picocli.CommandLine.Command
-import picocli.CommandLine.Option
 
-@Command(name = 'groovy-cli-graal-nativeimage-micronaut-example', description = '...',
-        mixinStandardHelpOptions = true)
-class GitopsPlaygroundCli implements Runnable {
+import groovy.cli.commons.CliBuilder
 
-    @Option(names = ['-v', '--verbose'], description = '...')
-    boolean verbose
+class GitopsPlaygroundCli {
 
     static void main(String[] args) throws Exception {
-        PicocliRunner.run(GitopsPlaygroundCli.class, args)
+        Configuration configuration = CommandLineInterface.INSTANCE.parse(args)
+    }
+}
+
+class Configuration {
+    private String greet
+
+    String getGreet() { return this.greet }
+
+    void setGreet(String greet) { this.greet = greet }
+}
+
+/**
+ * CLI-args definition and handling.
+ */
+enum CommandLineInterface {
+    INSTANCE
+    CliBuilder cliBuilder
+
+    CommandLineInterface() {
+        cliBuilder = new CliBuilder(
+                usage: 'e2e [<options>]',
+                header: 'Options:',
+                footer: 'And here we put footer text.'
+        )
+        // set the amount of columns the usage message will be wide
+        cliBuilder.width = 80  // default is 74
+        cliBuilder.with {
+            h longOpt: 'help', 'Print this help text and exit.'
+        }
     }
 
-    void run() {
-        // business logic here
-        if (verbose) {
-            println "Hi!"
+    Configuration parse(args) {
+        Configuration options = cliBuilder.parseFromInstance(Configuration.class, args)
+
+        if (!options) {
+            System.err << "Error while parsing command-line options.\n"
+            System.exit 1
         }
+
+        if (options.h) {
+            cliBuilder.usage()
+            System.exit 0
+        }
+
+        if (options.greet) {
+            println "Hi ..."
+            System.exit 0
+        }
+
+        return options
     }
 }
