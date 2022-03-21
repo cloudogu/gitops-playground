10aa6fa9733192dd0b4be5d552fce7007ec4719c
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2022-03-21 11:41:17 +0100

    Add test for proper exit code in failure

--- src/test/groovy/com/cloudogu/gitops/core/ApplicationTest.groovy
+++ src/test/groovy/com/cloudogu/gitops/core/ApplicationTest.groovy
@@ -3,13 +3,13 @@ package com.cloudogu.gitops.core
 import ch.qos.logback.classic.Level
 import com.cloudogu.gitops.core.modules.ModuleRepository
 import com.cloudogu.gitops.utils.TestLogger
+import com.github.stefanbirkner.systemlambda.SystemLambda
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 import static org.junit.jupiter.api.Assertions.assertEquals
 import static org.junit.jupiter.api.Assertions.assertTrue
-import static org.mockito.Mockito.mock
-import static org.mockito.Mockito.verify
+import static org.mockito.Mockito.*
 
 class ApplicationTest {
 
@@ -25,12 +25,26 @@ class ApplicationTest {
     }
 
     @Test
-    void testSucessfulInstallation() {
+    void 'application runs successfully'() {
         application.start()
 
         verify(moduleRepository).execute()
         assertEquals(testLogger.getLogs().size, 2)
         assertTrue(testLogger.getLogs().contains("Starting Application", Level.INFO))
-        assertTrue(testLogger.getLogs().contains("Application installed", Level.INFO))
+        assertTrue(testLogger.getLogs().contains("Application finished", Level.INFO))
+    }
+    
+    @Test
+    void 'application returns exit code 1 on exception'() {
+        when(moduleRepository.execute())
+                .thenThrow(new RuntimeException("Mocked"));
+
+        int status = SystemLambda.catchSystemExit(() -> {
+            application.start()
+        });
+
+        assertEquals(1, status);
+
+        assertTrue(testLogger.getLogs().contains("Application failed: Mocked", Level.ERROR))
     }
 }
