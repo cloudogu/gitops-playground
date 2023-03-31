b4cfa7f2d9c06a5bb67043d4993ef625dabd2003
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2023-03-31 14:15:58 +0200

    Simplify groovy() in apply-ng.
    Most params seem not to be necessary in our use case.

--- scripts/apply-ng.sh
+++ scripts/apply-ng.sh
@@ -15,24 +15,13 @@ function main() {
 
 function groovy() {
   # We don't need the groovy "binary" (script) to start, because the gitops-playground.jar already contains groovy-all.
-
-  # Write starter-conf file (required by groovy)
-  GROOVY_CONF=$(mktemp)
   
   # Set params like startGroovy does (which is called by the "groovy" script)
   # See https://github.com/apache/groovy/blob/master/src/bin/startGroovy
   java \
-    --add-modules=ALL-SYSTEM \
-    -Dgroovy.jaxb=jaxb \
     -classpath /app/gitops-playground.jar \
-    -Dscript.name="$0" \
-    -Dprogram.name="$(basename "$0")" \
-    -Dgroovy.starter.conf="$GROOVY_CONF" \
-    -Dgroovy.home=$PLAYGROUND_DIR \
     org.codehaus.groovy.tools.GroovyStarter \
           --main groovy.ui.GroovyMain \
-          --conf "$GROOVY_CONF" \
-          --classpath . \
            "$@" 
 }
 
