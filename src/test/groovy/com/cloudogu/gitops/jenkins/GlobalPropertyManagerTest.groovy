a398c39c1e58a6b1d24820f1fa9f69003c7762b2
Tim Siebels <tim.siebels@cloudogu.com>
2023-08-23 16:16:46 +0200

    Fix test

--- src/test/groovy/com/cloudogu/gitops/jenkins/GlobalPropertyManagerTest.groovy
+++ src/test/groovy/com/cloudogu/gitops/jenkins/GlobalPropertyManagerTest.groovy
@@ -21,11 +21,11 @@ class GlobalPropertyManagerTest {
             globalNodeProperties = instance.getGlobalNodeProperties()
             envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
             
-            newEnvVarsNodeProperty = null
-            envVars = null
+            def newEnvVarsNodeProperty
+            def envVars
             
             if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
-                newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
+                newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty()
                 globalNodeProperties.add(newEnvVarsNodeProperty)
                 envVars = newEnvVarsNodeProperty.getEnvVars()
             } else {
