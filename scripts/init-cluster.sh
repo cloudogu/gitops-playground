eeb58954c0d2f4311132daab6bb2a84b203ff26c
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2023-10-20 17:28:28 +0200

    init-cluster.sh: Improve tput handling
    else was missing and bash:3 container does not have tput, even though $TERM is set.

--- scripts/init-cluster.sh
+++ scripts/init-cluster.sh
@@ -205,14 +205,14 @@ readParameters() {
 function echoHightlighted() {
     # fallback to normal echo if TERM is not set
     # because tput requires a valid terminal
-    if [ -z "$TERM" ]; then
+    if [ -z "$TERM" ] || ! command -v tput > /dev/null 2>&1; then
         echo "$@"
+    else 
+      # Print to stdout in green
+      tput setaf 2
+      echo "$@"
+      tput sgr0
     fi
-
-    # Print to stdout in green
-    tput setaf 2
-    echo "$@"
-    tput sgr0
 }
 
 main "$@"
