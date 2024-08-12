0cb614dfb5460ffdfa830b7037505f7e4a7e0be8
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2024-08-12 08:59:09 +0200

    apply-ng.sh: Allow configuring classpath
    This is helpful when running GOP as scripts from dev image but the JAR
    resides somewhere else.
    
    For example, when applying GOP from a Jenkins job with docker.image
    .inside but mounting your own Groovy Scripts or when running locally
    
    CLASSPATH=target/xyz.jar scripts/apply-ng.sh

--- scripts/apply-ng.sh
+++ scripts/apply-ng.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
+TRACE=${TRACE:-}
+[[ $TRACE == true ]] && set -x;
+
 ABSOLUTE_BASEDIR="$(cd "$(dirname $0)" && pwd)"
 PLAYGROUND_DIR="$(cd ${ABSOLUTE_BASEDIR} && cd .. && pwd)"
+# Allow for overriding the folder to jar via env var
+export CLASSPATH="${CLASSPATH:-${PLAYGROUND_DIR}/gitops-playground.jar}"
 
 function apply-ng() {
   groovy --classpath "$PLAYGROUND_DIR"/src/main/groovy \
@@ -12,11 +17,11 @@ function apply-ng() {
 # Runs groovy files without needing groovy
 function groovy() {
   # We don't need the groovy "binary" (script) to start, because the gitops-playground.jar already contains groovy-all.
+  # Note that gitops-playground.jar is passed via env var CLASSPATH
 
   # Set params like startGroovy does (which is called by the "groovy" script)
   # See https://github.com/apache/groovy/blob/master/src/bin/startGroovy
   java \
-    -classpath "$PLAYGROUND_DIR"/gitops-playground.jar \
     org.codehaus.groovy.tools.GroovyStarter \
     --main groovy.ui.GroovyMain \
     "$@"
