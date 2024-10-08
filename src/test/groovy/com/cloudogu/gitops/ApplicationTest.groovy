47905a8cbad36292307ee2eb9f90ffe272c0b715
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2024-10-08 15:00:19 +0200

    Migrate Registry to using Config object

--- src/test/groovy/com/cloudogu/gitops/ApplicationTest.groovy
+++ src/test/groovy/com/cloudogu/gitops/ApplicationTest.groovy
@@ -1,28 +1,23 @@
 package com.cloudogu.gitops
 
 import com.cloudogu.gitops.config.Configuration
+import com.cloudogu.gitops.config.schema.Schema
 import io.micronaut.context.ApplicationContext
 import org.junit.jupiter.api.Test
 
 import static org.assertj.core.api.Assertions.assertThat
+import static com.cloudogu.gitops.config.schema.Schema.*
 
 class ApplicationTest {
-    Map config = [
-            application: [
-                    username: "the-user",
-                    password: "the-password"
-            ],
-            scmm: [
-                    internal: true,
-                    url: 'http://localhost'
-            ],
-            jenkins: [:]
-    ]
+
+    Schema config = new Schema(
+            scmm: new ScmmSchema(url: 'http://localhost'))
 
     @Test
     void 'feature\'s ordering is correct'() {
         def application = ApplicationContext.run()
-                .registerSingleton(new Configuration(config))
+                .registerSingleton(config)
+                .registerSingleton(new Configuration(config.toMap()))
                 .getBean(Application)
         def features = application.features.collect { it.class.simpleName }
 
