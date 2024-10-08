881f96a967d871942d44416cb49ccccdf19b44a0
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2024-10-08 15:00:19 +0200

    Migrate Registry to using Config object

--- src/test/groovy/com/cloudogu/gitops/features/RegistryTest.groovy
+++ src/test/groovy/com/cloudogu/gitops/features/RegistryTest.groovy
@@ -13,38 +13,21 @@ import org.junit.jupiter.api.Test
 
 import java.nio.file.Path
 
-import static org.assertj.core.api.Assertions.assertThat 
+import static org.assertj.core.api.Assertions.assertThat
+import static com.cloudogu.gitops.config.schema.Schema.*
 
 class RegistryTest {
 
-    Map config = [
-            registry   : [
-                    internal    : true,
-                    url         : '',
-                    path        : '',
-                    username    : '',
-                    password    : '',
-                    internalPort: Schema.DEFAULT_REGISTRY_PORT,
-                    helm        : [
-                            chart  : 'docker-registry',
-                            repoURL: 'https://charts.helm.sh/stable',
-                            version: '1.9.4'
-                    ]
-            ],
-            application: [
-                    namePrefix: "foo-",
-            ],
-    ]
-    K8sClientForTest k8sClient = new K8sClientForTest(config)
-    CommandExecutorForTest helmCommands = new CommandExecutorForTest()
-    HelmClient helmClient = new HelmClient(helmCommands)
+    K8sClientForTest k8sClient
+    CommandExecutorForTest helmCommands
+    HelmClient helmClient
     File temporaryYamlFile
 
     @Test
     void 'is disabled when external registry is configured'() {
-        config.registry['internal'] = false
-
-        createRegistry().install()
+        def registryConfig = new RegistrySchema(internal: false)
+        
+        createRegistry(registryConfig).install()
 
         assertThat(helmCommands.actualCommands).isEmpty()
         assertThat(k8sClient.commandExecutorForTest.actualCommands).isEmpty()
@@ -54,13 +37,13 @@ class RegistryTest {
     void 'is installed'() {
         createRegistry().install()
 
-        assertThat(parseActualYaml()['service']['nodePort']).isEqualTo(Schema.DEFAULT_REGISTRY_PORT)
+        assertThat(parseActualYaml()['service']['nodePort']).isEqualTo(DEFAULT_REGISTRY_PORT)
         assertThat(parseActualYaml()['service']['type']).isEqualTo('NodePort')
-        assertThat(helmCommands.actualCommands[0].trim()).isEqualTo(
-                'helm repo add registry https://charts.helm.sh/stable')
+        assertThat(helmCommands.actualCommands[0].trim()).startsWith(
+                'helm repo add registry')
         assertThat(helmCommands.actualCommands[1].trim()).startsWith(
                 'helm upgrade -i docker-registry registry/docker-registry --create-namespace')
-        assertThat(helmCommands.actualCommands[1].trim()).contains('--version 1.9.4')
+        assertThat(helmCommands.actualCommands[1].trim()).contains('--version')
         assertThat(helmCommands.actualCommands[1].trim()).contains("--values ${temporaryYamlFile}")
         assertThat(helmCommands.actualCommands[1].trim()).contains('--namespace foo-default')
         assertThat(k8sClient.commandExecutorForTest.actualCommands).isEmpty()
@@ -68,18 +51,26 @@ class RegistryTest {
 
     @Test
     void 'creates an additional service when different port is set'() {
-        def expectedNodePort = Schema.DEFAULT_REGISTRY_PORT as int + 1
-        config['registry']['internalPort'] = expectedNodePort
-
-        createRegistry().install()
+        def expectedNodePort = DEFAULT_REGISTRY_PORT as int + 1
+        def registryConfig = new RegistrySchema(internalPort: expectedNodePort)
+        
+        createRegistry(registryConfig).install()
 
         assertThat(k8sClient.commandExecutorForTest.actualCommands[0]).contains("--node-port $expectedNodePort")
     }
-    
-    private Registry createRegistry() {
-        // We use the real FileSystemUtils and not a mock to make sure file editing works as expected
 
-        new Registry(new Configuration(config), new FileSystemUtils() {
+    private Registry createRegistry(RegistrySchema registryConfig = new RegistrySchema()) {
+        def config = new Schema(
+                application: new ApplicationSchema(namePrefix: 'foo-'),
+                registry: registryConfig
+        )
+        Map configMap = config.toMap()
+        k8sClient = new K8sClientForTest(configMap)
+        helmCommands = new CommandExecutorForTest()
+        helmClient = new HelmClient(helmCommands)
+
+        // We use the real FileSystemUtils and not a mock to make sure file editing works as expected
+        new Registry(config, new FileSystemUtils() {
             @Override
             Path createTempFile() {
                 def ret = super.createTempFile()
@@ -87,11 +78,12 @@ class RegistryTest {
 
                 return ret
             }
-        }, k8sClient, new HelmStrategy(new Configuration(config), helmClient))
+        }, k8sClient, new HelmStrategy(new Configuration(configMap), helmClient))
     }
 
     private parseActualYaml() {
         def ys = new YamlSlurper()
         return ys.parse(temporaryYamlFile)
     }
+
 }
