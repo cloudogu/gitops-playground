a1599b9b5abfd66104eaf755328c393e3378766f
Niklas Hußmann <niklas.hussmann-extern@cloudogu.com>
2024-12-16 13:09:50 +0100

    adding maven failsafe plugin for integrationtests

---
+++ src/test/groovy/com/cloudogu/gitops/features/integration/PrometheusStackTestIT.groovy
@@ -0,0 +1,13 @@
+package com.cloudogu.gitops.features.integration
+
+import org.junit.jupiter.api.Test
+import static org.assertj.core.api.Assertions.assertThat
+
+class PrometheusStackTestIT {
+
+    @Test
+    void testtesttest()  {
+        assertThat(true).isEqualTo(false)
+
+    }
+}
