c2c5cd0ad8b18ddb579524c3cb4493480e9ba0ad
Thomas Michael <thomas.michael@cloudogu.com>
2024-12-18 13:10:36 +0100

    add IP direkt

--- src/test/groovy/com/cloudogu/gitops/integration/E2ETestIT.groovy
+++ src/test/groovy/com/cloudogu/gitops/integration/E2ETestIT.groovy
@@ -1,5 +1,6 @@
 package com.cloudogu.gitops.integration
 
+import com.cloudogu.gitops.utils.K8sClient
 import org.junit.jupiter.api.Test
 
 class E2ETestIT {
@@ -10,7 +11,7 @@ class E2ETestIT {
 
         def k3dAddress = System.getenv("K3D_ADDRESS") ?: "jenkins.localhost"
         E2EIT.main([
-                "--url", "http://jenkins.localhost",
+                "--url", "http://172.18.0.2:9090",
                 "--user", "admin",
                 "--password", "admin",
                 "--writeFailedLog",
