8f857a132c54c9ac3bd72b495d936837742d6f69
Anna Vetcininova <anna.vetcininova@cloudogu.com>
2025-09-08 14:12:15 +0200

    Remove GitHandler from ScmRepoProvider, because GitHandler is the access layer for the features(ArgoCD etc)

--- src/main/groovy/com/cloudogu/gitops/cli/GitopsPlaygroundCliMainScripted.groovy
+++ src/main/groovy/com/cloudogu/gitops/cli/GitopsPlaygroundCliMainScripted.groovy
@@ -57,7 +57,7 @@ class GitopsPlaygroundCliMainScripted {
 
             def httpClientFactory = new HttpClientFactory()
 
-            def scmmRepoProvider = new com.cloudogu.gitops.git.scmm.ScmRepoProvider(config, fileSystemUtils, null) // TODO: Anna
+            def scmmRepoProvider = new com.cloudogu.gitops.git.scmm.ScmRepoProvider(config, fileSystemUtils)
 
             def insecureSslContextProvider = new Provider<HttpClientFactory.InsecureSslContext>() {
                 @Override
