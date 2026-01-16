de3fbdf14e7b8fc77f53ba75c9420dc5e1cdbee6
Anna Vetcininova <anna.vetcininova@cloudogu.com>
2026-01-16 13:34:19 +0100

    Refactor gitOps repo multiTenant for consistency: central has now following repo structure: apps/argocd/applications  apps/argocd/projects
     instead of:
     apps/argocd/multiTenant/central/applications
     apps/argocd/multiTenant/central/projects

--- src/main/groovy/com/cloudogu/gitops/features/deployment/ArgoCdApplicationStrategy.groovy
+++ src/main/groovy/com/cloudogu/gitops/features/deployment/ArgoCdApplicationStrategy.groovy
@@ -134,13 +134,7 @@ class ArgoCdApplicationStrategy implements DeploymentStrategy {
                 ]
         ])
 
-        String appManifestPath="apps/argocd"
-        if (config.multiTenant.useDedicatedInstance) {
-            appManifestPath += "/multiTenant/central/applications/${releaseName}.yaml"
-        }else{
-            appManifestPath += "/applications/${releaseName}.yaml"
-        }
-
+        String appManifestPath="apps/argocd/applications/${releaseName}.yaml"
         clusterResourcesRepo.writeFile(appManifestPath, yamlResult)
 
         log.debug("Deploying helm release ${releaseName} basing on chart ${chartOrPath} from ${repoURL}, version " +
