ad25d36121365eb611cee8d660e411f7b45fce19
Niklas Hußmann <niklas.hussmann-extern@cloudogu.com>
2026-01-22 13:52:28 +0100

    test

--- src/main/groovy/com/cloudogu/gitops/config/Credentials.groovy
+++ src/main/groovy/com/cloudogu/gitops/config/Credentials.groovy
@@ -1,12 +1,25 @@
 package com.cloudogu.gitops.config
 
+import com.fasterxml.jackson.annotation.JsonPropertyDescription
+
+import static com.cloudogu.gitops.config.ConfigConstants.CONTENT_REPO_CREDENTIALS_DESCRIPTION
+
 class Credentials {
+    @JsonPropertyDescription(CONTENT_REPO_CREDENTIALS_DESCRIPTION)
     String username
+    @JsonPropertyDescription(CONTENT_REPO_CREDENTIALS_DESCRIPTION)
     String password
+    @JsonPropertyDescription(CONTENT_REPO_CREDENTIALS_DESCRIPTION)
     String secretNamespace
+    @JsonPropertyDescription(CONTENT_REPO_CREDENTIALS_DESCRIPTION)
     String secretName
+    @JsonPropertyDescription(CONTENT_REPO_CREDENTIALS_DESCRIPTION)
     String usernameKey
+    @JsonPropertyDescription(CONTENT_REPO_CREDENTIALS_DESCRIPTION)
     String passwordKey
+
+
+    Credentials() {}
 
     Credentials(String username, String password, String secretName = '', String secretNamespace = '', String usernameKey = "username", String passwordKey = 'password') {
         this.username = username
@@ -16,4 +29,17 @@ class Credentials {
         this.usernameKey = usernameKey
         this.passwordKey = passwordKey
     }
+
+    @Override
+    String toString() {
+        return "Credentials{" +
+                "username='" + username + '\'' +
+                ", password='*****'" +
+                ", secretNamespace='" + secretNamespace + '\'' +
+                ", secretName='" + secretName + '\'' +
+                ", usernameKey='" + usernameKey + '\'' +
+                ", passwordKey='" + passwordKey + '\'' +
+                '}';
+    }
+
 }
