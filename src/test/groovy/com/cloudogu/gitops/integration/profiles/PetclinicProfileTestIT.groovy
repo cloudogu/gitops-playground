beaaa4de4ca69cf6dc8ac574bf743b2316740b92
Thomas <thomas@thomasmichael.de>
2026-02-09 12:01:03 +0100

    using profiles for integrationstests (#342)
    * create profile tests and remove old tests and adopt jenkins
    
    * fix jenkinsfile
    
    * create k8s dump if any test fails
    
    * create test for operator-mandants profile
    
    * change condition for prefix Tests
    
    * bind registry cluster
    
    * bind registry cluster
    
    * ingress port changed
    
    * insecure test
    
    * test
    
    * Revert "test"
    
    This reverts commit f159a98d7853419d8fc802d36eb22315d5728781.
    
    * fixing bug
    
    * fixing bug
    
    * add finally block to ensure cluster deletion
    
    ---------
    
    Co-authored-by: Niklas Hußmann <niklas.hussmann-extern@cloudogu.com>

---
+++ src/test/groovy/com/cloudogu/gitops/integration/profiles/PetclinicProfileTestIT.groovy
@@ -0,0 +1,135 @@
+package com.cloudogu.gitops.integration.profiles
+
+import com.cloudogu.gitops.integration.TestK8sHelper
+import groovy.util.logging.Slf4j
+import io.fabric8.kubernetes.client.KubernetesClient
+import io.fabric8.kubernetes.client.KubernetesClientBuilder
+import io.fabric8.kubernetes.client.KubernetesClientException
+import org.awaitility.Awaitility
+import org.awaitility.core.ConditionTimeoutException
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.extension.RegisterExtension
+import org.junit.jupiter.api.extension.TestWatcher
+
+import java.util.concurrent.TimeUnit
+
+import static org.assertj.core.api.Assertions.assertThat
+import static org.assertj.core.api.Assertions.fail
+
+/**
+ * This tests can only be successfull, if one of theses profiles used.
+ *
+ * * To run locally: add -Dmicronaut.environments=content-examples to your execute configuration
+ */
+@Slf4j
+@EnabledIfSystemProperty(named = "micronaut.environments", matches = "full|operator-full|content-examples")
+class PetclinicProfileTestIT extends ProfileTestSetup {
+
+    static String exampleStagingNs = 'example-apps-staging'
+
+    @BeforeAll
+    static void labelTest() {
+        println "###### Testing Petclinic ######"
+        // petclinic need most of time to run. If online, we can start all tests.
+        try {
+            Awaitility.await()
+                    .atMost(20, TimeUnit.MINUTES)
+                    .pollInterval(5, TimeUnit.SECONDS)
+                    .untilAsserted {
+                        waitUntilPetclinicIsRunning()
+                    }
+        } catch (ConditionTimeoutException timeoutEx) {
+            TestK8sHelper.dumpNamespacesAndPods()
+            fail('Cluster not ready, sth false.', timeoutEx)
+        }
+    }
+    // Start condition
+    private static void waitUntilPetclinicIsRunning() {
+        // Check Pod
+        try (KubernetesClient client = new KubernetesClientBuilder().build()) {
+            def actualPods = client.pods().inNamespace(exampleStagingNs).list().getItems()
+            assert !actualPods.isEmpty(): "No pods found in petclinc - namespace: ${exampleStagingNs}"
+            def notRunningPods = actualPods.findAll { pod ->
+                pod.getStatus().getPhase() != "Running"
+            }
+            assert !actualPods.isEmpty() && notRunningPods.isEmpty(): "These pods in ${exampleStagingNs} are not yet running: ${notRunningPods.collect { it.getMetadata().getName() + ':' + it.getStatus().getPhase() }}"
+        }
+        catch (KubernetesClientException ex) {
+            fail("Unexpected Kubernetes exception", ex)
+        }
+    }
+
+
+    @Test
+    void ensurePetclinicIsRunningOnStages() {
+        try (KubernetesClient client = new KubernetesClientBuilder().build()) {
+
+            // Check Pod
+            def actualPods = client.pods().inNamespace(exampleStagingNs).list().getItems()
+
+            assert !actualPods.isEmpty(): "No pods found in petclinc - namespace: ${exampleStagingNs}"
+
+            def notRunningPods = actualPods.findAll { pod ->
+                pod.getStatus().getPhase() != "Running"
+            }
+
+            assert notRunningPods.isEmpty(): "These pods in ${exampleStagingNs} are not yet running: ${notRunningPods.collect { it.getMetadata().getName() + ':' + it.getStatus().getPhase() }}"
+
+        } catch (KubernetesClientException ex) {
+            fail("Unexpected Kubernetes exception", ex)
+        }
+    }
+
+    @DisabledIfSystemProperty(named = "micronaut.environments", matches = "full|operator-full|content-examples")
+// operator can not install nginx
+    @Test
+    void ensurePetclinicIngressIsOnline() {
+        try (KubernetesClient client = new KubernetesClientBuilder().build()) {
+            def nameOfServiceAndIngress = "spring-petclinic-plain"
+            // check Ingress
+            def ingress = client.network()
+                    .v1()
+                    .ingresses()
+                    .inNamespace(exampleStagingNs)
+                    .withName(nameOfServiceAndIngress)
+                    .get()
+
+            assert ingress != null: "Ingress '${nameOfServiceAndIngress}' not found in '${exampleStagingNs}'"
+
+            def hosts = (ingress.spec?.rules ?: [])
+                    .collect { it?.host }
+                    .findAll { it }
+
+            assert hosts.get(0).contains("petclinic") // in this case, petclinic do not care about prefix
+        } catch (KubernetesClientException ex) {
+            fail("Unexpected Kubernetes exception", ex)
+        }
+    }
+
+    @DisabledIfSystemProperty(named = "micronaut.environments", matches = "full|operator-full|content-examples")
+// operator can not install nginx
+    @Test
+    void ensurePetclinicServidsdsdceIsOnline() {
+        try (KubernetesClient client = new KubernetesClientBuilder().build()) {
+
+            // Check Service
+            def nameOfServiceAndIngress = "spring-petclinic-plain"
+            def service = client.services()
+                    .inNamespace(exampleStagingNs)
+                    .withName(nameOfServiceAndIngress)
+                    .get()
+
+            assertThat(service).isNotNull()
+
+
+        } catch (KubernetesClientException ex) {
+            fail("Unexpected Kubernetes exception", ex)
+        }
+    }
+
+}
