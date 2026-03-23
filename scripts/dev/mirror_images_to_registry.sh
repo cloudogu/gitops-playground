dec8483a387e953692719e76cd14d94e6cba0d54
David Daehne <david.daehne@cloudogu.com>
2026-03-23 15:54:51 +0100

    make mirror_images_to_registry.sh more versatile

--- scripts/dev/mirror_images_to_registry.sh
+++ scripts/dev/mirror_images_to_registry.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -xeo pipefail
 
+# This scripts copies images needed for testing purposes into an harbor instance running at $1
+# Usage: ./mirror_images_to_registry.sh <HARBOR_BASE_URL>
+
+HARBOR_BASE_URL=$1
+HARBOR_DOCKER_BASE_URL=docker:$(echo $HARBOR_BASE_URL | cut -d: -f2-)
+
 MAILHOG_IMAGE="docker://ghcr.io/cloudogu/mailhog:v1.0.1"
 ESO_IMAGE="docker://ghcr.io/external-secrets/external-secrets:v0.9.16"
 VAULT_IMAGE="docker://hashicorp/vault:1.14.0"
@@ -24,7 +30,7 @@ MVN_IMAGE="docker://maven:3-eclipse-temurin-17-alpine"
 YAMLLINT_IMAGE="docker://cytopia/yamllint:1.25-0.7"
 
 # Hit the API to see when harbor is ready
-until curl -s -o /dev/null -w "%{http_code}" http://localhost:30000/api/v2.0/projects | grep -q "200"; do
+until curl -s -o /dev/null -w "%{http_code}" $HARBOR_BASE_URL/api/v2.0/projects | grep -q "200"; do
     echo "Waiting for harbor"
     sleep 1
 done
@@ -42,46 +48,46 @@ for operation in "${operations[@]}"; do
     lower_operation=$(echo "$operation" | tr '[:upper:]' '[:lower:]')
 
     echo "creating project ${lower_operation}"
-    projectId=$(curl -is --fail 'http://localhost:30000/api/v2.0/projects' -X POST -u admin:Harbor12345 -H 'Content-Type: application/json' --data-raw "{\"project_name\":\"$lower_operation\",\"metadata\":{\"public\":\"false\"},\"storage_limit\":-1,\"registry_id\":null}" | grep -i 'Location:' | awk '{print $2}' | awk -F '/' '{print $NF}' | tr -d '[:space:]')
+    projectId=$(curl -is --fail "$HARBOR_BASE_URL/api/v2.0/projects" -X POST -u admin:Harbor12345 -H 'Content-Type: application/json' --data-raw "{\"project_name\":\"$lower_operation\",\"metadata\":{\"public\":\"false\"},\"storage_limit\":-1,\"registry_id\":null}" | grep -i 'Location:' | awk '{print $2}' | awk -F '/' '{print $NF}' | tr -d '[:space:]')
 
     echo creating user ${operation} with PW ${operation}12345
-    curl -s  --fail 'http://localhost:30000/api/v2.0/users' -X POST -u admin:Harbor12345 -H 'Content-Type: application/json' --data-raw "{\"username\":\"$operation\",\"email\":\"$operation@example.com\",\"realname\":\"$operation example\",\"password\":\"${operation}12345\",\"comment\":null}"
+    curl -s  --fail "$HARBOR_BASE_URL/api/v2.0/users" -X POST -u admin:Harbor12345 -H 'Content-Type: application/json' --data-raw "{\"username\":\"$operation\",\"email\":\"$operation@example.com\",\"realname\":\"$operation example\",\"password\":\"${operation}12345\",\"comment\":null}"
 
     echo "Adding member ${operation} to project ${lower_operation}; ID=${projectId}"
-    curl --fail "http://localhost:30000/api/v2.0/projects/${projectId}/members" -X POST -u admin:Harbor12345 -H 'Content-Type: application/json' --data-raw "{\"role_id\":${roles['maintainer']},\"member_user\":{\"username\":\"$operation\"}}"
+    curl --fail "$HARBOR_BASE_URL/api/v2.0/projects/${projectId}/members" -X POST -u admin:Harbor12345 -H 'Content-Type: application/json' --data-raw "{\"role_id\":${roles['maintainer']},\"member_user\":{\"username\":\"$operation\"}}"
 done
 
 echo "creating user ${readOnlyUser} with PW ${readOnlyUser}12345"
-curl -s  --fail 'http://localhost:30000/api/v2.0/users' -X POST -u admin:Harbor12345 -H 'Content-Type: application/json' --data-raw "{\"username\":\"$readOnlyUser\",\"email\":\"$readOnlyUser@example.com\",\"realname\":\"$readOnlyUser example\",\"password\":\"${readOnlyUser}12345\",\"comment\":null}"
+curl -s  --fail "$HARBOR_BASE_URL/api/v2.0/users" -X POST -u admin:Harbor12345 -H 'Content-Type: application/json' --data-raw "{\"username\":\"$readOnlyUser\",\"email\":\"$readOnlyUser@example.com\",\"realname\":\"$readOnlyUser example\",\"password\":\"${readOnlyUser}12345\",\"comment\":null}"
 echo "Adding member ${readOnlyUser} to project proxy; ID=${projectId}"
-curl  --fail "http://localhost:30000/api/v2.0/projects/${projectId}/members" -X POST -u admin:Harbor12345 -H 'Content-Type: application/json' --data-raw "{\"role_id\":${roles['limited-guest']},\"member_user\":{\"username\":\"${readOnlyUser}\"}}"
+curl  --fail "$HARBOR_BASE_URL/api/v2.0/projects/${projectId}/members" -X POST -u admin:Harbor12345 -H 'Content-Type: application/json' --data-raw "{\"role_id\":${roles['limited-guest']},\"member_user\":{\"username\":\"${readOnlyUser}\"}}"
 
 
 # When updating the container image versions note that all images of a chart are listed at artifact hub on the right hand side under "Containers Images"
-skopeo copy $MAILHOG_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  docker://localhost:30000/proxy/mailhog
-skopeo copy $ESO_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  docker://localhost:30000/proxy/external-secrets
-skopeo copy $VAULT_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  docker://localhost:30000/proxy/vault
-skopeo copy $NGINX_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  docker://localhost:30000/proxy/nginx
-skopeo copy $TRAEFIK_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false docker://localhost:30000/proxy/traefik
+skopeo copy $MAILHOG_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  $HARBOR_DOCKER_BASE_URL/proxy/mailhog
+skopeo copy $ESO_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  $HARBOR_DOCKER_BASE_URL/proxy/external-secrets
+skopeo copy $VAULT_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  $HARBOR_DOCKER_BASE_URL/proxy/vault
+skopeo copy $NGINX_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  $HARBOR_DOCKER_BASE_URL/proxy/nginx
+skopeo copy $TRAEFIK_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false $HARBOR_DOCKER_BASE_URL/proxy/traefik
 
 # Monitoring
 # Using latest will lead to failure with
 # k describe prometheus -n monitoring
 #  Message:               initializing PrometheusRules failed: failed to parse version: Invalid character(s) found in major number "0latest"
-skopeo copy $PROMETHEUS_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false docker://localhost:30000/proxy/prometheus
-skopeo copy $PROMETHEUS_OPERATOR_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false docker://localhost:30000/proxy/prometheus-operator
-skopeo copy $PROMETHEUS_OPERATOR_CONFIG_RELOADER --dest-creds Proxy:Proxy12345 --dest-tls-verify=false docker://localhost:30000/proxy/prometheus-config-reloader
-skopeo copy $GRAFANA_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false docker://localhost:30000/proxy/grafana
-skopeo copy $K8S_SIDECAR --dest-creds Proxy:Proxy12345 --dest-tls-verify=false docker://localhost:30000/proxy/k8s-sidecar
+skopeo copy $PROMETHEUS_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false $HARBOR_DOCKER_BASE_URL/proxy/prometheus
+skopeo copy $PROMETHEUS_OPERATOR_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false $HARBOR_DOCKER_BASE_URL/proxy/prometheus-operator
+skopeo copy $PROMETHEUS_OPERATOR_CONFIG_RELOADER --dest-creds Proxy:Proxy12345 --dest-tls-verify=false $HARBOR_DOCKER_BASE_URL/proxy/prometheus-config-reloader
+skopeo copy $GRAFANA_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false $HARBOR_DOCKER_BASE_URL/proxy/grafana
+skopeo copy $K8S_SIDECAR --dest-creds Proxy:Proxy12345 --dest-tls-verify=false $HARBOR_DOCKER_BASE_URL/proxy/k8s-sidecar
 
 # Cert Manager images
-skopeo copy $CERT_MANAGER_CONTROLLER --dest-creds Proxy:Proxy12345 --dest-tls-verify=false docker://localhost:30000/proxy/cert-manager-controller
-skopeo copy $CERT_MANAGER_CA_INJECTOR --dest-creds Proxy:Proxy12345 --dest-tls-verify=false docker://localhost:30000/proxy/cert-manager-cainjector
-skopeo copy $CERT_MANAGER_WEBHOOK --dest-creds Proxy:Proxy12345 --dest-tls-verify=false docker://localhost:30000/proxy/cert-manager-webhook
+skopeo copy $CERT_MANAGER_CONTROLLER --dest-creds Proxy:Proxy12345 --dest-tls-verify=false $HARBOR_DOCKER_BASE_URL/proxy/cert-manager-controller
+skopeo copy $CERT_MANAGER_CA_INJECTOR --dest-creds Proxy:Proxy12345 --dest-tls-verify=false $HARBOR_DOCKER_BASE_URL/proxy/cert-manager-cainjector
+skopeo copy $CERT_MANAGER_WEBHOOK --dest-creds Proxy:Proxy12345 --dest-tls-verify=false $HARBOR_DOCKER_BASE_URL/proxy/cert-manager-webhook
 
 # Needed for the builds to work with proxy-registry
-skopeo copy $KUBECTL_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  docker://localhost:30000/proxy/bitnami/kubectl:1.29
-skopeo copy $TEMURIN_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  docker://localhost:30000/proxy/eclipse-temurin:17-jre-alpine
-skopeo copy $HELM_IMAGE  --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  docker://localhost:30000/proxy/helm:latest
-skopeo copy $MVN_IMAGE  --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  docker://localhost:30000/proxy/maven:3-eclipse-temurin-17-alpine
-skopeo copy $YAMLLINT_IMAGE  --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  docker://localhost:30000/proxy/yamllint:latest
+skopeo copy $KUBECTL_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  $HARBOR_DOCKER_BASE_URL/proxy/bitnami/kubectl:1.29
+skopeo copy $TEMURIN_IMAGE --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  $HARBOR_DOCKER_BASE_URL/proxy/eclipse-temurin:17-jre-alpine
+skopeo copy $HELM_IMAGE  --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  $HARBOR_DOCKER_BASE_URL/proxy/helm:latest
+skopeo copy $MVN_IMAGE  --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  $HARBOR_DOCKER_BASE_URL/proxy/maven:3-eclipse-temurin-17-alpine
+skopeo copy $YAMLLINT_IMAGE  --dest-creds Proxy:Proxy12345 --dest-tls-verify=false  $HARBOR_DOCKER_BASE_URL/proxy/yamllint:latest
--- scripts/dev/prepare_airgapped_cluster.sh
+++ scripts/dev/prepare_airgapped_cluster.sh
@@ -3,42 +3,17 @@ set -xeu pipefail
 # This script will setup a cluster to test an air-gapped environment with
 # It copies all needed container images for an airgapped-environment-test into a harbor instance
 
-
-# Add more images here, if you like
-# We're not adding registry, scmm, jenkins and argocd here, because we have to install them before we go offline (see bellow for details).
-IMAGE_PATTERNS=('external-secrets' \
-  'vault' \
-  'prometheus' \
-  'grafana' \
-  'sidecar' \
-  'traefik')
-BASIC_SRC_IMAGES=$(
-  kubectl get pods --all-namespaces -o jsonpath="{range .items[*]}{range .spec.containers[*]}{'\n'}{.image}{end}{end}" \
-  | grep -Ff <(printf "%s\n" "${IMAGE_PATTERNS[@]}") \
-  | sed 's/docker\.io\///g' | sort | uniq)
-BASIC_DST_IMAGES=''
-
 # create cluster
 k3d cluster delete gitops-playground
 ./scripts/init-cluster.sh --cluster-name=airgapped-playground
 
 K3D_NODE=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' k3d-airgapped-playground-server-0)
 
-# Now init some apps you want to have running (e.g. harbor) before going airgapped
-helm upgrade  -i my-harbor harbor/harbor --version 1.12.2 --namespace harbor -f ./scripts/dev/external-registry-values.yaml --set externalURL=http://$K3D_NODE:30002 --create-namespace
-
-# waiting for harbor to come online
-until curl -sf http://$K3D_NODE:30002 > /dev/null; do sleep 1; done
-
+helm repo add harbor https://helm.goharbor.io
+helm upgrade -i my-harbor harbor/harbor --version 1.14.2 --namespace harbor --create-namespace  --values ./scripts/dev/external-registry-values.yaml --set externalURL=http://$K3D_NODE:30002
 
 # Switch context to airgapped cluster here, e.g.
 sed -i -r "s/0.0.0.0([^0-9]+[0-9]*|\$)/${K3D_NODE}:6443/g" ~/.config/k3d/kubeconfig-airgapped-playground.yaml
 export KUBECONFIG=$HOME/.config/k3d/kubeconfig-airgapped-playground.yaml
 
-while IFS= read -r image; do
-  dstImage=$K3D_NODE:30002/library/${image##*/}
-  echo pushing image $image to $dstImage
-  skopeo copy docker://$image --dest-creds admin:Harbor12345 --dest-tls-verify=false  docker://$dstImage
-  BASIC_DST_IMAGES+="${dstImage}\n"
-done <<< "$BASIC_SRC_IMAGES"
-echo $BASIC_DST_IMAGES
+./scripts/dev/mirror_images_to_registry.sh http://$K3D_NODE:30002
