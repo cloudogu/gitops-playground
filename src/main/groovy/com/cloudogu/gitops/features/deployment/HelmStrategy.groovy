821506cc684cdfdf975392ff32e18af4f823c62e
Thomas <thomas@thomasmichael.de>
2026-03-30 11:08:08 +0200

    Update src/main/groovy/com/cloudogu/gitops/features/deployment/HelmStrategy.groovy
    Co-authored-by: Niklas Hußmann <niklas.hussmann-extern@cloudogu.com>

--- src/main/groovy/com/cloudogu/gitops/features/deployment/HelmStrategy.groovy
+++ src/main/groovy/com/cloudogu/gitops/features/deployment/HelmStrategy.groovy
@@ -35,7 +35,7 @@ class HelmStrategy implements DeploymentStrategy {
                     "version ${version}, into namespace ${namespace}. Using values:\n${helmValuesPath.toFile().text}")
 
             if (RepoType.HELM  == repoType ) {
-                 log.debug("helm chart by http")
+                log.debug("adding helm repo {}.",repoUrl)
                 helmClient.addRepo(repoName, repoURL)
             helmClient.upgrade(releaseName, "$repoName/$chartOrPath",
                     [namespace: namespace,
