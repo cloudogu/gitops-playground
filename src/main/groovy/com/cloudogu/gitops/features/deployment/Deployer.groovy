b7df2cfdd41b170d6baaa4f85af7100a5ddee74a
Niklas Hußmann <niklas.hussmann-extern@cloudogu.com>
2026-04-23 15:40:14 +0200

    refactoring Dockerfile

--- src/main/groovy/com/cloudogu/gitops/features/deployment/Deployer.groovy
+++ src/main/groovy/com/cloudogu/gitops/features/deployment/Deployer.groovy
@@ -1,30 +1,33 @@
 package com.cloudogu.gitops.features.deployment
 
-import com.cloudogu.gitops.features.deployment.DeploymentStrategy.RepoType
+import com.cloudogu.gitops.config.Config
+
+import io.micronaut.context.annotation.Primary
 
 import java.nio.file.Path
+import jakarta.inject.Singleton
 
-import org.jvnet.hk2.annotations.Service
+@Singleton
+@Primary
+class Deployer implements DeploymentStrategy {
+	private Config config
+	private ArgoCdApplicationStrategy argoCdStrategy
+	private HelmStrategy helmStrategy
 
-@Service
-class Deployer {
-
-	@Lazy
-	ArgoCdApplicationStrategy argoCdStrategy
-
-	HelmStrategy helmStrategy
-
-	Deployer(HelmStrategy helmStrategy) {
+	Deployer(Config config, ArgoCdApplicationStrategy argoCdStrategy, HelmStrategy helmStrategy) {
 		this.helmStrategy = helmStrategy
+		this.argoCdStrategy = argoCdStrategy
+		this.config = config
 	}
 
+	@Override
 	void deployFeature(
 			String repoURL, String repoName, String chartOrPath, String version, String namespace,
-			String releaseName, Path helmValuesPath, RepoType repoType, boolean initByHelm = false) {
-
-		if (initByHelm) {
+			String releaseName, Path helmValuesPath, RepoType repoType) {
+		if (config.features['argocd']['active']) {
+			argoCdStrategy.deployFeature(repoURL, repoName, chartOrPath, version, namespace, releaseName, helmValuesPath, repoType)
+		} else {
 			helmStrategy.deployFeature(repoURL, repoName, chartOrPath, version, namespace, releaseName, helmValuesPath, repoType)
 		}
-		argoCdStrategy.deployFeature(repoURL, repoName, chartOrPath, version, namespace, releaseName, helmValuesPath, repoType)
 	}
 }
