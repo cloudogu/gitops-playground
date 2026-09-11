de3f365faaf6a18e16ef1e3d013d75ca34e00d67
Anna Vetcininova <anna.vetcininova@cloudogu.com>
2026-09-11 14:59:52 +0200

    Remove VersionName annotation processor

--- src/main/java/com/cloudogu/gitops/cli/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Creates class Version during build via annotation processing
- */
-@VersionName(packageName = "com.cloudogu.gitops.cli")
-package com.cloudogu.gitops.cli;
-
-import com.cloudogu.versionname.VersionName;
